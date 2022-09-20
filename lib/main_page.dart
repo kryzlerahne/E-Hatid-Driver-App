0748cf6f652f68aadd70310fb010694be0e8bf97
kryzle <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-09-21 03:28:18 +0800

    Modified and created Main Page Dart as Connector

---
+++ lib/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:ehatid_driver_app/Screens/login.dart';
+import 'package:ehatid_driver_app/homepage.dart';
+import 'package:ehatid_driver_app/intro_slider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StreamBuilder<User?>(stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return LoginScreen();
+        }
+        },
+        ),
+    );
+  }
+}
