62ad15f118f3d2a5e371320661ae3bae2e73b127
kryzle <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-09-28 13:40:36 +0800

    Google Map Driver

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ehatid_driver_app/homepage.dart';
+/**import 'package:ehatid_driver_app/homepage.dart';
 import 'package:ehatid_driver_app/login.dart';
 import 'package:ehatid_driver_app/order_traking_page.dart';
 import 'package:flutter/material.dart';
@@ -42,4 +42,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
+**/
 
+import 'package:ehatid_driver_app/navigation_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:ehatid_driver_app/navigation_screen.dart';
+
+main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController latController = TextEditingController();
+  TextEditingController lngController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter uber'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Enter your location',
+            style: TextStyle(fontSize: 40),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          TextField(
+            controller: latController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'latitude',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: lngController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'longitute',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => NavigationScreen(
+                          double.parse(latController.text),
+                          double.parse(lngController.text))));
+                },
+                child: Text('Get Directions')),
+          ),
+        ]),
+      ),
+    );
+  }
+}
