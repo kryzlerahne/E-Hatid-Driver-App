5b03b93dbb47c22950beff3dae2b857e61935b41
kryzle <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-09-29 20:54:56 +0800

    Sliding Up Panel

--- lib/accept_decline.dart
+++ lib/accept_decline.dart
@@ -140,13 +140,14 @@ class AcceptDeclineState extends State<AcceptDecline> {
 
   @override
   Widget build(BuildContext context) {
-    final paneHeightClosed = MediaQuery.of(context).size.height * 0.4;
+    final paneHeightClosed = MediaQuery.of(context).size.height * 0.12;
     final paneHeightOpen = MediaQuery.of(context).size.height * 0.45;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFFED90F),
-        title: Text('Trip Details'),
+        title: Text('Trip Details',
+        style:TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.bold)),
         elevation: 0,
         leading: IconButton(
           onPressed: () {
@@ -177,7 +178,7 @@ class AcceptDeclineState extends State<AcceptDecline> {
           controller: controller,
           panelController: panelController,
         ),
-        borderRadius: BorderRadius.vertical(top: Radius.circular(18)),
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
       ),
     );
   }
@@ -210,7 +211,132 @@ class PanelWidget extends StatelessWidget {
    child: Column(
      crossAxisAlignment: CrossAxisAlignment.start,
      children: <Widget>[
-       Text("Text Here")
+       Row(
+         //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         children: <Widget> [
+           Icon(Icons.account_circle,
+             size: 40,
+           color: Color(0xFF272727),
+           ),
+          SizedBox(width: 20,),
+           Column(
+             children: [
+               Row(
+                 children: [
+                   Text("Karlo Pangilinan ",
+                     style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,
+                     ),
+                   ),
+                   Icon(Icons.verified_rounded,
+                     color: Color(0xFF0CBC8B),),
+                 ],
+               ),
+               Row(
+                 children: [
+                   SizedBox(width: 10,),
+                   Text("Verified Passenger",
+                   style: TextStyle(fontFamily: 'Montserrat', fontSize: 15,
+                   ),
+                   ),
+                   SizedBox(width: 50,),
+                 ],
+               ),
+             ],
+           ),
+           Spacer(),
+           Icon(Icons.call,
+             size: 40,
+             color: Color(0xFF0CBC8B),
+           ),
+         ],
+       ),
+       SizedBox(height: 25,),
+       Row(
+         children: [
+           SizedBox(width: 15,),
+           Image.asset("assets/images/line.png"),
+           SizedBox(width: 30,),
+           Column(
+             mainAxisAlignment: MainAxisAlignment.end,
+             children: [
+               Text("Pickup Point:",
+               style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w600,color: Color(0XFF9E9E9E)),),
+               Text("7-Eleven, Bolbok, Batangas City",
+               style: TextStyle(fontFamily: 'Montserrat',),), //PLACE YOUR LOCATION HERE
+               SizedBox(height: 10,),
+               Center(
+                 child: Container(
+                   width: 280,
+                   height: 2,
+                   decoration: BoxDecoration(
+                     color: Color(0XFFCCC9C1),
+                     borderRadius: BorderRadius.circular(32),
+                   ),
+                 ),
+               ),
+               SizedBox(height: 10,),
+               Text("Destination:",
+                 style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w600, color: Color(0XFFFFBA4C)),),
+               Text("Sta. Rita de Cascia Parish Church",
+               style: TextStyle(fontFamily: 'Montserrat',),),
+             ],
+           ),
+         ],
+       ),
+       SizedBox(height: 30,),
+       Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         children: [
+           Text("Distance",
+            style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
+           ),
+           Text("Time",
+            style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
+           ),
+         ],
+       ),
+       Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         children: [
+           //SizedBox(height: 20,),
+           Text("192m away",
+             style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,color: Color(0XFFFFBA4C)),
+           ),
+           Text("15 mins",
+             style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600, color: Color(0XFFFFBA4C)),
+           ),
+         ],
+       ),
+       SizedBox(height: 20, width: 20,),
+       Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         children: [
+           MaterialButton(
+             onPressed: (){},
+             shape: RoundedRectangleBorder(
+                 borderRadius: BorderRadius.circular(50)
+             ),
+             minWidth: 150,
+             child: Text("Continue", style: TextStyle( color: Colors.white,
+               fontSize: 15,
+               fontFamily: "Montserrat",
+               fontWeight: FontWeight.w600,),),
+             color: Color(0XFF0CBC8B),
+           ),
+           MaterialButton(
+             onPressed: (){},
+             shape: RoundedRectangleBorder(
+                 borderRadius: BorderRadius.circular(50)
+             ),
+             minWidth: 150,
+             child: Text("Cancel", style: TextStyle( color: Colors.white,
+               fontSize: 15,
+               fontFamily: "Montserrat",
+               fontWeight: FontWeight.w600,),),
+             color: Color(0XFFC5331E),
+           ),
+         ],
+       ),
      ],
    ),
  );
