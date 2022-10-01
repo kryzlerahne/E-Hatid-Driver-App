c4356473446203d3a67d84595f0aad8ad4c22483
kryzle <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-10-01 19:51:33 +0800

    Responsive Sizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:ehatid_driver_app/accept_decline.dart';
+import 'package:ehatid_driver_app/homescreen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'intro_slider.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:flutter/services.dart';
 import 'navigation_bar.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'navigation_screen.dart';
 
 int initScreen = 0;
@@ -22,97 +24,28 @@ Future <void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({
+  const MyApp({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        backgroundColor: Color(0xFFFED90F),
-      ),
-      //home: IntroSliderPage(),
-      initialRoute: initScreen == 0 ? 'introslider' : 'homepage', // paltan ang login ng homepage
-      routes: {
-        'homepage' : (context) => Navigation(),
-        'introslider' : (context) => IntroSliderPage(),
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            backgroundColor: Color(0xFFFED90F),
+          ),
+          //home: IntroSliderPage(),
+          initialRoute: initScreen == 0 ? 'introslider' : 'homepage',
+          // paltan ang login ng homepage
+          routes: {
+            'homepage': (context) => Navigation(),
+            'introslider': (context) => IntroSliderPage(),
+          },
+        );
       },
     );
   }
 }
-
-
-/**
-import 'package:ehatid_driver_app/navigation_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:ehatid_driver_app/navigation_screen.dart';
-
-main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController latController = TextEditingController();
-  TextEditingController lngController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter uber'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            'Enter your location',
-            style: TextStyle(fontSize: 40),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          TextField(
-            controller: latController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'latitude',
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            controller: lngController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'longitute',
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => NavigationScreen(
-                          double.parse(latController.text),
-                          double.parse(lngController.text))));
-                },
-                child: Text('Get Directions')),
-          ),
-        ]),
-      ),
-    );
-  }
-}
- **/
