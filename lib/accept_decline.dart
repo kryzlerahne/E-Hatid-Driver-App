ac8c418d2aab252f03360bbc4472b3abed536e1d
kryzle <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-10-02 14:08:53 +0800

    Responsive Accept Decline

--- lib/accept_decline.dart
+++ lib/accept_decline.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:ehatid_driver_app/navigation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 List cars = [
@@ -50,8 +51,8 @@ class AcceptDeclineState extends State<AcceptDecline> {
 
   @override
   Widget build(BuildContext context) {
-    final paneHeightClosed = MediaQuery.of(context).size.height * 0.12;
-    final paneHeightOpen = MediaQuery.of(context).size.height * 0.45;
+    final paneHeightClosed = Adaptive.h(11);
+    final paneHeightOpen = Adaptive.h(45);
 
     return Scaffold(
       appBar: AppBar(
@@ -110,9 +111,9 @@ class PanelWidget extends StatelessWidget {
     padding: EdgeInsets.zero,
     controller: controller,
     children: <Widget>[
-      SizedBox(height: 12),
+      SizedBox(height: Adaptive.h(1)),
       buildDragHandle(),
-      SizedBox(height: 18),
+      SizedBox(height: Adaptive.h(1.5)),
       buildAboutText(context),
     ],
   );
@@ -122,53 +123,57 @@ class PanelWidget extends StatelessWidget {
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Row(
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget> [
-            Icon(Icons.account_circle,
-              size: 40,
-              color: Color(0xFF272727),
-            ),
-            SizedBox(width: 20,),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Text("Karlo Pangilinan ",
-                      style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,
+        Container(
+          height: Adaptive.h(7),
+          width: Adaptive.w(100),
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget> [
+              Icon(Icons.account_circle,
+                size: 40,
+                color: Color(0xFF272727),
+              ),
+              SizedBox(width: Adaptive.w(2),),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("Karlo Pangilinan ",
+                        style: TextStyle(fontFamily: 'Montserrat', fontSize: 4.3.w, fontWeight: FontWeight.w600,
+                        ),
                       ),
-                    ),
-                    Icon(Icons.verified_rounded,
-                      color: Color(0xFF0CBC8B),),
-                  ],
-                ),
-                Row(
-                  children: [
-                    SizedBox(width: 10,),
-                    Text("Verified Passenger",
-                      style: TextStyle(fontFamily: 'Montserrat', fontSize: 15,
+                      Icon(Icons.verified_rounded,
+                        color: Color(0xFF0CBC8B),),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(width: Adaptive.w(2),),
+                      Text("Verified Passenger",
+                        style: TextStyle(fontFamily: 'Montserrat', fontSize: 3.6.w,
+                        ),
                       ),
-                    ),
-                    SizedBox(width: 50,),
-                  ],
-                ),
-              ],
-            ),
-            Spacer(),
-            Icon(Icons.call,
-              size: 40,
-              color: Color(0xFF0CBC8B),
-            ),
-          ],
+                      SizedBox(width: 50,),
+                    ],
+                  ),
+                ],
+              ),
+              Spacer(),
+              Icon(Icons.call,
+                size: 40,
+                color: Color(0xFF0CBC8B),
+              ),
+            ],
+          ),
         ),
-        SizedBox(height: 25,),
+        SizedBox(height: Adaptive.h(1),),
         FittedBox(
           fit: BoxFit.fill,
           child: Row(
             children: [
-              SizedBox(width: 30,),
-              Image.asset("assets/images/line.png"),
-              SizedBox(width: 30,),
+              SizedBox(width: Adaptive.w(3),),
+              Image.asset("assets/images/line.png", height: Adaptive.h(16),),
+              SizedBox(width: Adaptive.w(8),),
               Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
@@ -176,7 +181,7 @@ class PanelWidget extends StatelessWidget {
                     style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w600,color: Color(0XFF9E9E9E)),),
                   Text("7-Eleven, Bolbok, Batangas City",
                     style: TextStyle(fontFamily: 'Montserrat',),), //PLACE YOUR LOCATION HERE
-                  SizedBox(height: 10,),
+                  SizedBox(height: Adaptive.h(2),),
                   Center(
                     child: Container(
                       width: 280,
@@ -187,7 +192,7 @@ class PanelWidget extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(height: Adaptive.h(1),),
                   Text("Destination:",
                     style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w600, color: Color(0XFFFFBA4C)),),
                   Text("Sta. Rita de Cascia Parish Church",
@@ -197,37 +202,45 @@ class PanelWidget extends StatelessWidget {
             ],
           ),
         ),
-        SizedBox(height: 20,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text("Distance",
-              style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
-            ),
-            Text("Time",
-              style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
-            ),
-          ],
+        SizedBox(height: Adaptive.h(1),),
+        Container(
+          width: Adaptive.w(90),
+          height: Adaptive.h(7),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Distance",
+                    style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
+                  ),
+                  Text("Time",
+                    style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //SizedBox(height: 20,),
+                  Text("192m away",
+                    style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,color: Color(0XFFFFBA4C)),
+                  ),
+                  Text("15 mins",
+                    style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600, color: Color(0XFFFFBA4C)),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            //SizedBox(height: 20,),
-            Text("192m away",
-              style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,color: Color(0XFFFFBA4C)),
-            ),
-            Text("15 mins",
-              style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600, color: Color(0XFFFFBA4C)),
-            ),
-          ],
-        ),
-        SizedBox(height: 20, width: 20,),
-        FittedBox(
-          fit: BoxFit.fill,
+        SizedBox(height: Adaptive.h(2),),
+        Align(
+          alignment: Alignment.bottomCenter,
           child: Row(
             //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              SizedBox(width: 30,),
+              SizedBox(width: Adaptive.w(2),),
               MaterialButton(
                 onPressed: (){
                   showDialog(
@@ -238,21 +251,21 @@ class PanelWidget extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(50)
                 ),
-                minWidth: 150,
+                minWidth: Adaptive.w(40),
                 child: Text("Continue", style: TextStyle( color: Colors.white,
                   fontSize: 15,
                   fontFamily: "Montserrat",
                   fontWeight: FontWeight.w600,),),
                 color: Color(0XFF0CBC8B),
               ),
-              SizedBox(width: 10,),
+              SizedBox(width: Adaptive.w(2),),
               MaterialButton(
                 onPressed: (){
                 },
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(50)
                 ),
-                minWidth: 150,
+                minWidth: Adaptive.w(40),
                 child: Text("Cancel", style: TextStyle( color: Colors.white,
                   fontSize: 15,
                   fontFamily: "Montserrat",
@@ -269,8 +282,8 @@ class PanelWidget extends StatelessWidget {
   Widget buildDragHandle() => GestureDetector(
     child: Center(
       child: Container(
-        width: 50,
-        height: 5,
+        width: Adaptive.w(15),
+        height: Adaptive.h(0.5),
         decoration: BoxDecoration(
           color: Color(0XFFD1D1D1),
           borderRadius: BorderRadius.circular(32),
@@ -332,47 +345,45 @@ class BookingSuccessDialog extends StatelessWidget {
                 ),
               ),
             ),
-            Expanded(
-              child: Form(
-                child: Container(
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("PHP",
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          color: Colors.black,
-                          fontSize: 20,
-                          letterSpacing: -0.5,
-                          fontWeight: FontWeight.bold,
+            Form(
+              child: Container(
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("PHP",
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.black,
+                        fontSize: 20,
+                        letterSpacing: -0.5,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Flexible(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.32,
+                        child: TextField(
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(hintText: "Fare Price",
+                            hintStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                            //isCollapsed: true,
+                            contentPadding: EdgeInsets.symmetric(horizontal: 10),),
                         ),
                       ),
-                      Flexible(
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.32,
-                          child: TextField(
-                            textAlign: TextAlign.center,
-                            decoration: InputDecoration(hintText: "Fare Price",
-                              hintStyle: TextStyle(
-                                fontFamily: 'Montserrat',
-                                color: Colors.black,
-                                fontSize: 18,
-                              ),
-                              //isCollapsed: true,
-                              contentPadding: EdgeInsets.symmetric(horizontal: 10),),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                SizedBox(height: 40,),
+                SizedBox(height: Adaptive.h(7),),
                 Text("Base Fare:",
                   style: TextStyle(
                     fontFamily: 'Montserrat',
@@ -380,7 +391,7 @@ class BookingSuccessDialog extends StatelessWidget {
                     fontSize: 14,
                   ),
                 ),
-                SizedBox(width: 150,),
+                SizedBox(width: Adaptive.w(34),),
                 Text("P40.00",
                   style: TextStyle(
                     fontFamily: 'Montserrat',
@@ -390,7 +401,7 @@ class BookingSuccessDialog extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 10,),
+           // SizedBox(height: Adaptive.h(2),),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -401,7 +412,7 @@ class BookingSuccessDialog extends StatelessWidget {
                     fontSize: 14,
                   ),
                 ),
-                SizedBox(width: 130,),
+                SizedBox(width: Adaptive.w(30),),
                 Text("P10.00",
                   style: TextStyle(
                     fontFamily: 'Montserrat',
@@ -411,18 +422,18 @@ class BookingSuccessDialog extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 30,),
+            SizedBox(height: Adaptive.h(3),),
             Center(
               child: Container(
-                width: 260,
-                height: 1.5,
+                width: Adaptive.w(60),
+                height: Adaptive.h(.2),
                 decoration: BoxDecoration(
                   color: Color(0XFFCCC9C1),
                   borderRadius: BorderRadius.circular(32),
                 ),
               ),
             ),
-            SizedBox(height: 15,),
+            SizedBox(height: Adaptive.h(2),),
             Center(
               child: Text(
                 "Don't worry! Additional charges may apply\n if cancellation of booking is done by\n the passenger.",
@@ -434,25 +445,23 @@ class BookingSuccessDialog extends StatelessWidget {
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.pushReplacement(context, MaterialPageRoute(
-                    builder: (_) => DriverMap(),
-                  ),
-                  );
-                },
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50)
+            SizedBox(height: Adaptive.h(2),),
+            MaterialButton(
+              onPressed: () {
+                Navigator.pushReplacement(context, MaterialPageRoute(
+                  builder: (_) => DriverMap(),
                 ),
-                minWidth: 150,
-                child: Text("Get Directions", style: TextStyle( color: Colors.white,
-                  fontSize: 15,
-                  fontFamily: "Montserrat",
-                  fontWeight: FontWeight.w600,),),
-                color: Color(0XFF0CBB8A),
+                );
+              },
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)
               ),
+              minWidth: Adaptive.w(50),
+              child: Text("Get Directions", style: TextStyle( color: Colors.white,
+                fontSize: 15,
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.w600,),),
+              color: Color(0XFF0CBB8A),
             ),
           ],
         ),
