d9693d374d1f7dfd0b26c429cc3ccec4c4d4e20d
Cesar Edgardo Bicol Jr <102768040+Zephynity@users.noreply.github.com>
2022-10-04 10:54:25 +0800

    Driver Map

--- lib/navigation_screen.dart
+++ lib/navigation_screen.dart
@@ -4,9 +4,11 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:location/location.dart' as loc;
 import 'package:location/location.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:math' show cos, sqrt, asin;
 
+import 'accept_decline.dart';
 import 'main.dart';
 
 class DriverMap extends StatefulWidget {
@@ -117,6 +119,11 @@ class _NavigationScreenState extends State<NavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Home"),
+        backgroundColor: Color(0xFFFED90F),
+      ),
       body: sourcePosition == null
           ? Center(child: CircularProgressIndicator())
           : Stack(
@@ -149,30 +156,192 @@ class _NavigationScreenState extends State<NavigationScreen> {
             ),
           ),
           Positioned(
-              bottom: 10,
-              right: 10,
-              child: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.blue),
-                child: Center(
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.navigation_outlined,
-                      color: Colors.white,
-                    ),
-                    onPressed: () async {
-                      await launchUrl(Uri.parse(
-                          'google.navigation:q=${widget.lat}, ${widget.lng}&key=AIzaSyCGZt0_a-TM1IKRQOLJCaMJsV0ZXuHl7Io'));
-                    },
+            bottom: 10,
+            right: 10,
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.blue),
+              child: Center(
+                child: IconButton(
+                  icon: Icon(
+                    Icons.navigation_outlined,
+                    color: Colors.white,
                   ),
+                  onPressed: () async {
+                    await launchUrl(Uri.parse(
+                        'google.navigation:q=${widget.lat}, ${widget.lng}&key=AIzaSyCGZt0_a-TM1IKRQOLJCaMJsV0ZXuHl7Io'));
+                  },
                 ),
-              ))
+              ),
+            ),
+          ),
+          Positioned(
+            right: 30.w,
+            top: 20.h,
+            child: Container(
+              height: Adaptive.h(5),
+              width: Adaptive.h(22),
+              child: GoOnline(context),
+            ),
+          ),
         ],
       ),
     );
   }
+
+  Widget GoOnline(BuildContext context) => FloatingActionButton.extended(
+    label: Text('I have arrived',
+      style: TextStyle(fontFamily: 'Montserrat', fontSize: 12),
+    ),
+    backgroundColor: Color(0xFF0CBC8B),
+    onPressed: (){
+      showDialog(
+        context: context,
+        builder: (context) => Dialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(19)
+          ),
+          child: Container(
+            height: 45.h,
+            child: Column(
+              children: [
+                Container(
+                  height: 53.0,
+                  decoration: BoxDecoration(
+                    color: Color(0XFF0CBB8A),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.redAccent.withOpacity(0.2),
+                        spreadRadius: 3,
+                        blurRadius: 3,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: SizedBox.expand(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Karlo Pangilinan",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.white,
+                              fontSize: 5.3.w,
+                              letterSpacing: -0.5,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 1.w,),
+                          Icon(Icons.verified_rounded,
+                            color: Colors.white,),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: Adaptive.h(2),),
+                FittedBox(
+                  fit: BoxFit.fill,
+                  child: Row(
+                    children: [
+                      SizedBox(width: Adaptive.w(1),),
+                      Image.asset("assets/images/line.png", height: Adaptive.h(16),),
+                      SizedBox(width: Adaptive.w(1.5),),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text("Pickup Point:",
+                            style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w600,color: Color(0XFF9E9E9E)),),
+                          Text("7-Eleven, Bolbok, Batangas City",
+                            style: TextStyle(fontFamily: 'Montserrat',),), //PLACE YOUR LOCATION HERE
+                          SizedBox(height: Adaptive.h(2),),
+                          Center(
+                            child: Container(
+                              width: 60.w,
+                              height: 2,
+                              decoration: BoxDecoration(
+                                color: Color(0XFFCCC9C1),
+                                borderRadius: BorderRadius.circular(32),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: Adaptive.h(1),),
+                          Text("Destination:",
+                            style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w600, color: Color(0XFFFFBA4C)),),
+                          Text("Sta. Rita de Cascia Parish Church",
+                            style: TextStyle(fontFamily: 'Montserrat',),),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: Adaptive.h(3),),
+                Container(
+                  width: Adaptive.w(90),
+                  height: Adaptive.h(7),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("Distance",
+                            style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
+                          ),
+                          Text("Time",
+                            style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          //SizedBox(height: 20,),
+                          Text("192m away",
+                            style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600,color: Color(0XFFFFBA4C)),
+                          ),
+                          Text("15 mins",
+                            style: TextStyle(fontFamily: 'Montserrat', fontSize: 18, fontWeight: FontWeight.w600, color: Color(0XFFFFBA4C)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: Adaptive.h(2),),
+                MaterialButton(
+                  onPressed: (){
+                    showDialog(
+                      context: context,
+                      builder: (context) => BookingSuccessDialog(),
+                    );
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  minWidth: Adaptive.w(40),
+                  child: Text("Start Trip", style: TextStyle( color: Colors.white,
+                    fontSize: 15,
+                    fontFamily: "Montserrat",
+                    fontWeight: FontWeight.w600,),),
+                  color: Color(0XFF0CBC8B),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+    icon: Icon(Icons.beenhere, size: 17),
+  );
 
   getNavigation() async {
     bool _serviceEnabled;
